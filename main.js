805c053dccf5cdb09abd79a78686068a63f70f0f
kendallli587 <kendallli587@gmail.com>
2022-07-20 09:51:23 -0600

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ let goBtnEl = document.getElementById('go-btn');
 let menuEl = document.getElementById('menu');
 let outputEl = document.getElementById('output');
 
+// Global Variables
+let contacts = loadTasks;
+
 // Go Btn - Menu Listener
 goBtnEl.addEventListener('click', goBtnHandler);
 
@@ -27,11 +30,21 @@ function goBtnHandler() {
 
 // MENU FUNCTIONS
 function displayContacts() {
-  console.log('Display Contacts');
+  let outputStr = "";
+  for (let i = 0; i < contacts.length; i++) {
+    outputStr += getContactHTMLStr(contacts[i]);
+  }
+  outputEl.innerHTML = outputStr;
 }
 
 function addContact() {
-  console.log('Add Contact');
+  let name = prompt("Enter the contact's name: ");
+  let email = prompt(`Enter ${name}'s email address: `);
+  let phoneNum = prompt(`Enter ${name}'s phone number: `);
+  let country = prompt(`Enter ${name}'s country: `);
+
+  contacts.push(newContact(name, email, phoneNum, country));
+  outputEl.innerHTML = `Contact ${name} has been successfully added.`;
 }
 
 function removeContact() {
@@ -44,4 +57,33 @@ function displayByName() {
 
 function displayByCountry() {
   console.log('Display by Country');
+}
+
+// HELPER FUNCTIONS
+function newContact(name, email, number, country) {
+  return {
+    name: name,
+    email: email,
+    phoneNum: number,
+    country: country
+  };
+}
+
+function getContactHTMLStr(contact) {
+  return `
+  <div>
+    ${contact.name}
+    ${contact.email}
+    ${contact.phoneNum} (${contact.country})
+  </div>
+  `;
+}
+
+function saveTasks() {
+  localStorage.setItem("contacts", JSON.stringify(contacts));
+}
+
+function loadTasks() {
+  let contactStr = localStorage.getItem("tasks");
+  return JSON.parse(contactStr) ?? [];
 }
